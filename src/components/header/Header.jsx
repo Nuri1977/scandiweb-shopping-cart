f1c5f5d95a3d077c60692156bb0cf7913da6d0f3
Nuri1977 <nurilacka@gmail.com>
2022-07-18 08:31:38 +0200

    Create header and connect to redux store

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,77 @@
-import React, { Component } from 'react'
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import {
+  selectIsCartMenuOpen,
+  toggleCartMenu,
+  dismissCartMenu,
+} from "../../redux/cart.reducer";
+
+import { ReactComponent as CartSVG } from "../../assets/empty-cart.svg";
+import { ReactComponent as LogoSVG } from "../../assets/logo.svg";
+import { ReactComponent as ArrowSVG } from "../../assets/down-arrow.svg";
+
 import "./Header.scss";
 
-export class Header extends Component {
+import { Link } from "react-router-dom";
+
+class Header extends React.Component {
+  dismissAllMenus = () => {
+    if (this.props.isCartMenuOpen) this.props.dismissCartMenu();
+  };
+
+  componentDidMount() {
+    window.addEventListener("click", this.dismissAllMenus);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("click", this.dismissAllMenus);
+  }
+
   render() {
+    const { isCartMenuOpen, toggleCartMenu} = this.props;
+
+    const handleOnClickCartIcon = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log(isCartMenuOpen)
+      toggleCartMenu();
+    };
+
     return (
-      <div>Header.comp</div>
-    )
+      <header>
+        <nav>
+          <Link to="/">WOMEN</Link>
+          <Link to="/">MEN</Link>
+          <Link to="/">KIDS</Link>
+        </nav>
+
+        <div className="logo">
+          <LogoSVG />
+        </div>
+
+        <div className="menus">
+          <div className="currency-icon">
+            <span>$</span>
+            <ArrowSVG />
+          </div>
+          <div className="cart-icon" onClick={handleOnClickCartIcon}>
+            <CartSVG />
+          </div>
+        </div>
+      </header>
+    );
   }
 }
 
-export default Header
+const mapStateToProps = createStructuredSelector({
+  isCartMenuOpen: selectIsCartMenuOpen,
+});
+
+const mapDispatchToState = (dispatch) => ({
+  toggleCartMenu: () => dispatch(toggleCartMenu()),
+  dismissCartMenu: () => dispatch(dismissCartMenu()),
+});
+
+export default connect(mapStateToProps, mapDispatchToState)(Header);
