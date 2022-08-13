a185a018a0fa04595424eb07842e1ddca7b85754
Nuri1977 <nurilacka@gmail.com>
2022-08-13 14:11:25 +0200

    Fix Category data fetching

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -16,13 +16,27 @@ import "./Category.scss";
 import CategoryItem from "../../components/categoryItem/CategoryItem";
 
 class Category extends React.Component {
-  render() {
+
+  componentDidMount() {
     const { categoryName, getProducts, fetchCategoryProducts } = this.props;
+    if (!getProducts(categoryName)) {
+      fetchCategoryProducts(categoryName);
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const { categoryName, fetchCategoryProducts } = this.props;
+    if (categoryName !== prevProps.categoryName) {
+      fetchCategoryProducts(categoryName);
+    }
+  }
+
+  render() {
+    const { categoryName, getProducts } = this.props;
     const products = getProducts(categoryName);
-    if (!products) fetchCategoryProducts(categoryName);
 
     return (
-      <div id="category-page">
+      <div id="category-page" key={categoryName}>
         <h1>{categoryName}</h1>
         {products ? (
           <div className="products-container">
